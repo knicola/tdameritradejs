769faf7086c5c935e76bfe019611a55987fbb9c3
Kyriakos Nicola <knicola@users.noreply.github.com>
2020-07-27 21:34:55 -0400

    add level one equity transform

--- src/streamer/transforms.js
+++ src/streamer/transforms.js
@@ -101,6 +101,67 @@ function timesale(data) {
     })
 } // timesale()
 
+function levelOneEquity(data) {
+    return Object.assign({}, data, {
+        content: data.content.map(entry => {
+            return {
+                key: entry.key,
+                assetMainType: entry.assetMainType,
+                assetSubType: entry.assetSubType,
+                cusip: entry.cusip,
+                delayed: entry.delayed,
+                bidPrice: entry[1],
+                askPrice: entry[2],
+                lastPrice: entry[3],
+                bidSize: entry[4],
+                askSize: entry[5],
+                askID: entry[6],
+                bidID: entry[7],
+                totalVolume: entry[8],
+                lastSize: entry[9],
+                tradeTime: entry[10],
+                quoteTime: entry[11],
+                highPrice: entry[12],
+                lowPrice: entry[13],
+                bidTick: entry[14],
+                closePrice: entry[15],
+                exchangeID: entry[16],
+                marginable: entry[17],
+                shortable: entry[18],
+                quoteDay: entry[22],
+                tradeDay: entry[23],
+                volatility: entry[24],
+                description: entry[25],
+                lastID: entry[26],
+                digits: entry[27],
+                openPrice: entry[28],
+                netChange: entry[29],
+                '52WeekHigh': entry[30],
+                '52WeekLow': entry[31],
+                peRatio: entry[32],
+                dividendAmount: entry[33],
+                dividendYield: entry[34],
+                nav: entry[37],
+                fundPrice: entry[38],
+                exchangeName: entry[39],
+                dividendDate: entry[40],
+                regularMarketQuote: entry[41],
+                regularMarketTrade: entry[42],
+                regularMarketLastPrice: entry[43],
+                regularMarketLastSize: entry[44],
+                regularMarketTradeTime: entry[45],
+                regularMarketTradeDay: entry[46],
+                regularMarketNetChange: entry[47],
+                securityStatus: entry[48],
+                mark: entry[49],
+                quoteTimeInLong: entry[50],
+                tradeTimeInLong: entry[51],
+                regularMarketTradeTimeInLong: entry[52],
+            }
+        })
+    })
+} // levelOneEquity()
+
 module.exports = {
     accountActivity,
     // chartEquity,
@@ -108,4 +169,5 @@ module.exports = {
     chartFuturesOptions,
     newsHeadline,
     timesale,
+    levelOneEquity,
 }
